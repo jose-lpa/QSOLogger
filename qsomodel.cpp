ee8ce8b4ac8110f447e1760c8c5961407264436f
José L. Patiño <jose.lpa@gmail.com>
2019-08-31 01:43:12 +0100

    Implemented `QSOModel` class.

---
+++ qsomodel.cpp
@@ -0,0 +1,102 @@
+#include <QMessageBox>
+#include <QSqlError>
+#include <QSqlField>
+
+#include "qsomodel.h"
+
+
+QSOModel::QSOModel(QObject *parent)
+    : QSqlTableModel { parent }
+{
+    setTable("record");
+    select();
+    setHeaderData(0, Qt::Horizontal, tr("ID"));
+    setHeaderData(1, Qt::Horizontal, tr("Time"));
+    setHeaderData(2, Qt::Horizontal, tr("Call sign"));
+    setHeaderData(3, Qt::Horizontal, tr("Band"));
+    setHeaderData(4, Qt::Horizontal, tr("Frequency"));
+    setHeaderData(5, Qt::Horizontal, tr("Mode"));
+    setHeaderData(6, Qt::Horizontal, tr("Power"));
+    setHeaderData(7, Qt::Horizontal, tr("Signal TX"));
+    setHeaderData(8, Qt::Horizontal, tr("Signal RX"));
+    setHeaderData(9, Qt::Horizontal, tr("Grid TX"));
+    setHeaderData(10, Qt::Horizontal, tr("Grid RX"));
+    setHeaderData(11, Qt::Horizontal, tr("Name"));
+    setHeaderData(12, Qt::Horizontal, tr("Notes"));
+}
+
+void QSOModel::addQSO(const QDateTime &dateTime, const QString &callSign,
+            const QString &name, const QString &band, const QString &mode,
+            const double &frequency, const double &power,
+            const QString &signalTx, const QString &signalRx,
+            const QString &gridTx, const QString &gridRx,
+            const QString &notes)
+{
+    QSqlField dateTimeField("timestamp", QVariant::Int);
+    dateTimeField.setValue(dateTime.currentSecsSinceEpoch());
+
+    QSqlField callSignField("callsign", QVariant::String);
+    callSignField.setValue(callSign);
+
+    QSqlField nameField("name", QVariant::String);
+    nameField.setValue(name);
+
+    QSqlField bandField("band", QVariant::String);
+    bandField.setValue(band);
+
+    QSqlField modeField("mode", QVariant::String);
+    modeField.setValue(mode);
+
+    QSqlField frequencyField("frequency", QVariant::Double);
+    frequencyField.setValue(frequency);
+
+    QSqlField powerField("power", QVariant::Double);
+    powerField.setValue(power);
+
+    QSqlField signalTxField("signal_tx", QVariant::Int);
+    signalTxField.setValue(signalTx);
+
+    QSqlField signalRxField("signal_rx", QVariant::Int);
+    signalRxField.setValue(signalRx);
+
+    QSqlField gridTxField("grid_tx", QVariant::String);
+    gridTxField.setValue(gridTx);
+
+    QSqlField gridRxField("grid_rx", QVariant::String);
+    gridRxField.setValue(gridRx);
+
+    QSqlField notesField("notes", QVariant::String);
+    notesField.setValue(notes);
+
+    QSqlRecord record;
+
+    record.append(dateTimeField);
+    record.append(callSignField);
+    record.append(nameField);
+    record.append(bandField);
+    record.append(modeField);
+    record.append(frequencyField);
+    record.append(powerField);
+    record.append(signalTxField);
+    record.append(signalRxField);
+    record.append(gridTxField);
+    record.append(gridRxField);
+    record.append(notesField);
+
+    if (!insertRecord(-1, record))
+    {
+        QMessageBox errorMessage;
+        errorMessage.setWindowTitle(tr("QSO Logger error"));
+        errorMessage.setText(lastError().text());  // FIXME: no `lastError` here.
+        errorMessage.setIcon(QMessageBox::Critical);
+        errorMessage.exec();
+    }
+    else
+    {
+        QMessageBox successMessage;
+        successMessage.setWindowTitle(tr("QSO Added"));
+        successMessage.setText(tr("New QSO successfully created."));
+        successMessage.setIcon(QMessageBox::Information);
+        successMessage.exec();
+    }
+}
---
+++ qsomodel.h
@@ -0,0 +1,23 @@
+#ifndef QSOMODEL_H
+#define QSOMODEL_H
+
+#include <QDateTime>
+#include <QSqlTableModel>
+#include <QSqlRecord>
+
+
+class QSOModel : public QSqlTableModel
+{
+    Q_OBJECT
+
+public:
+    QSOModel(QObject *parent = nullptr);
+    void addQSO(const QDateTime &dateTime, const QString &callSign,
+                const QString &name, const QString &band, const QString &mode,
+                const double &frequency, const double &power,
+                const QString &signalTx, const QString &signalRx,
+                const QString &gridTx, const QString &gridRx,
+                const QString &notes);
+};
+
+#endif // QSOMODEL_H
